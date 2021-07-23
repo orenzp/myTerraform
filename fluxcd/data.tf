dcaeb457c9e45577f56b1ce4ef5fa0cdac81f439
Oren Zipori <orenzp@hotmail.com>
2021-07-23 12:01:08 +0300

    Create initial files for FluxCD Terraform configuration

---
+++ fluxcd/data.tf
@@ -0,0 +1,17 @@
+data "kubectl_file_documents" "install" {
+  content = data.flux_install.main.content
+}
+
+data "kubectl_file_documents" "sync" {
+  content = data.flux_sync.main.content
+}
+
+data "flux_install" "main" {
+  target_path = var.target_path
+}
+
+data "flux_sync" "main" {
+  target_path = var.target_path
+  url         = "ssh://git@github.com/${var.github_owner}/${var.repository_name}.git"
+  branch      = var.branch
+}
