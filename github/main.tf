9a6e578c3a5aee690b4f5b01a680ba1efdf7919a
Oren Zipori <orenzp@hotmail.com>
2021-07-25 18:08:11 +0300

    Update myGithub workspace with new github repo for augury interview

--- github/main.tf
+++ github/main.tf
@@ -1,10 +1,12 @@
 resource "github_repository" "terraform-github-repo" {
   name               = "terraform-github-repo"
-  description        = "Terraform module for creating Github Repositories"
+  description        = "Terraform module for creating Github Repositories, This module was created via my Terraform Cloud workspace"
   has_issues         = false
   auto_init          = true
   gitignore_template = "Terraform"
   license_template   = "mit"
   has_projects       = false
+  has_wiki           = false
   visibility         = "public"
+  topics             = ["terraform-module", "github"]
 }
